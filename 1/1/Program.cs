96c5ef8d6b7ccff5c118f00eef3de0f0a897c030
schu8762 <J.Schuller@ou.edu>
2019-09-26 11:48:40 -0500

    2

---
+++ 1/1/Program.cs
@@ -0,0 +1,42 @@
+﻿//@author Jacob Schuller
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] courses = new string[3];
+            double[] coursenumb = new double[3];
+            // double[] coursenumb = { };       
+            //string coursenumb = Console.ReadLine();
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine($"Enter course subject ");
+                courses[i] = Console.ReadLine();
+
+                //Convert.ToString(courses)
+                Console.WriteLine($"Enter course number ");
+                coursenumb[i] = Convert.ToDouble(Console.ReadLine());
+
+                //string courses = Console.ReadLine();
+            }
+
+            for (int i = 0; i < courses.Length; i++)
+            {
+                Console.WriteLine(courses[i] + coursenumb[i]);
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2/2/Program.cs
@@ -0,0 +1,44 @@
+﻿//@author Jacob Schuller
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            //Console.WriteLine("Please enter in your grade");
+           // double grades = Convert.ToDouble(Console.ReadLine());
+         
+            List<double> userGrades = new List<double>();
+            string answer;
+
+            do
+            {
+                Console.WriteLine("Please enter in your grade");
+                userGrades.Add(Convert.ToDouble(Console.ReadLine()));
+
+
+                Console.WriteLine("Do you have another grade? y/n");
+                answer = Console.ReadLine();
+
+
+            } while (answer != "n");
+
+            foreach (var grade in userGrades)
+            {
+                double sum = (grade + grade);
+            }
+
+            Console.WriteLine(sum);
+
+            Console.ReadKey();
+
+        }
+    }
+}
